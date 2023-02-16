6037e109562707412fa480e248f16ce0cab9d2b8
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2023-02-16 09:34:04 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logoJWBlanc from "./image/LogoJWBlanc.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoJWBlanc} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jwellweb.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Voir le site JwellWeb
         </a>
       </header>
     </div>
